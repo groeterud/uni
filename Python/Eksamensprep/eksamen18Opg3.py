a8b3aa4884f694831ac5d1fa18361a968dd31a2a
groeterud <groeterud@users.noreply.github.com>
2020-11-06 17:27:21 +0100

    yass

--- Python/Eksamensprep/eksamen18Opg3.py
+++ Python/Eksamensprep/eksamen18Opg3.py
@@ -1,9 +1,7 @@
-import tkinter
+from tkinter import *
 
-fortsette=True
-
-while fortsette==True:
-    manedslonn=int(input('Oppgi månedslønn i kr: '))
+def beregn_skatt():
+    manedslonn=int(manedslonn_in.get())
 
     if manedslonn<=20000:
         skatt=0.00
@@ -20,11 +18,43 @@ while fortsette==True:
                     skatt=0.48
 
     utbetalt=manedslonn*(1-skatt)
-    print('Du får utbetalt',format(utbetalt,'.2f'),'kr per måned og skatteprosenten er',int(skatt*100),'%')
+    utbetalt_out.set(utbetalt)
 
-    svar=input('Skal du utføre flere lønnsberegninger (Ja/ja)? ')
+def avslutt():
+    window.destroy()
 
-    if svar=='Ja' or svar=='ja':
-        fortsette=True
-    else:
-        fortsette=False
+window=Tk()
+
+window.title('Skatteberegning')
+
+#variabler
+manedslonn_in=StringVar()
+utbetalt_out=StringVar()
+
+#ledetekster 
+lbl_manedslonn=Label(window,text='Månedslønn:')
+lbl_utbetalt=Label(window,text='Utvetalt')
+
+#knapper
+btn_beregn=Button(window,text='Beregn',width=15,command=beregn_skatt)
+btn_avslutt=Button(window,text='Avslutt',width=5,command=avslutt)
+
+#entrys
+ent_manedslonn=Entry(window,width=10,textvariable=manedslonn_in)
+output_utbetalt=Entry(window,width=10,state='readonly',textvariable=utbetalt_out)
+
+#plassering
+#ledetekster
+lbl_manedslonn.grid(row=0,column=0,pady=15,sticky=E)
+lbl_utbetalt.grid(row=1,column=0,pady=15,sticky=E)
+
+#knapper
+btn_beregn.grid(row=0,column=2,padx=10)
+btn_avslutt.grid(row=2,column=2,sticky=E,padx=10,pady=10)
+
+#entrys
+ent_manedslonn.grid(row=0,column=1,padx=15,pady=15,sticky=E)
+output_utbetalt.grid(row=1,column=1,padx=15,pady=15,sticky=E)
+
+
+window.mainloop()

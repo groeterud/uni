b6123f803c7399f9f626e608cb8062892c139093
groeterud <groeterud@users.noreply.github.com>
2020-11-13 20:29:12 +0100

    OWNING
    Co-authored-by: Boss Kadomo <240199@usn.no>

--- Python/Eksamensprep/eksamen18Opg2.py
+++ Python/Eksamensprep/eksamen18Opg2.py
@@ -7,6 +7,11 @@ print(l2)
 print(nyliste)
 print()
 
+
+#your job: combine the 2 lists
+#The values are to be added with the smalles value first according to the index of x in both lists.
+#assume both lists are of the same length.
+
 for x in range (0,len(l1),1):
     if l1[x]<=l2[x]:
         nyliste+=[l1[x]]
